174bc13697c6861f0feb802d643e4ceb065fcd1d
Theplayer13 <lost.108.00@mail.ru>
2024-09-18 18:20:03 +0300

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,5 @@
+Number_of_completed_tasks = '12'
+Number_of_hours_spent = '1.5'
+Course_title = 'Python'
+Time_per_task = '0.125'
+print('Курс:',Course_title,'всего задач:',Number_of_completed_tasks,'затрачено часов:',Number_of_hours_spent,'среднее время выполнения',Time_per_task,'часа')
